2c389469b4fafa5400cf1681bb322d07f6cc863c
Amaaddd <137601336+Amaaddd@users.noreply.github.com>
2023-07-02 23:43:43 +0530

    Add files via upload

---
+++ Ass7.py
@@ -0,0 +1,19 @@
+def file_operation(roll_no, name,stud_class, file="data.txt"):
+    try:
+        f=open(file, "w")
+        pass
+        f=open(file,"a")
+        line=[f"{roll_no}, {name}, {stud_class}"]
+        f.writelines(line)
+        f=open(file,"r")
+        data=f.readlines()
+        print("Data is: ")
+
+        print(data)
+
+    except FileNotFoundError:
+        print(f"{file} was not found....enter correct file name")
+
+    except Exception:
+        print("Something went wrong")
+file_operation(42, "Amaad", "3rd Year")
